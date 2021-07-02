29b21b81838cf14b9c945629c6a7fa03c8a1fce9
Madhawa <madhawa@intervest.lk>
2021-07-02 18:05:40 +0530

    support for multiple browsers

--- src/test/java/Setup/PetsureSetup.java
+++ src/test/java/Setup/PetsureSetup.java
@@ -65,7 +65,7 @@ public class PetsureSetup {
     @AfterMethod
     public void teardown() {
         if (driver != null) {
-//            driver.quit();
+            driver.quit();
         }
     }
 
